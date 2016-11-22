e81aa3f5778bf763b1f10fd40a55f35e3f50f77e
Tetsu <tksgi12@gmail.com>
2016-11-22 23:02:45 +0900

    外観完成

--- numberPuzzle2048/config/initializers/assets.rb
+++ numberPuzzle2048/config/initializers/assets.rb
@@ -9,3 +9,5 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( pz2048.css )
+Rails.application.config.assets.precompile += %w( puzzle/pz2048.js )
--- numberPuzzle2048/config/routes.rb
+++ numberPuzzle2048/config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'home/top'
+  get '2048'=>'home#puzzle2048'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
